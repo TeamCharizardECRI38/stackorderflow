73423fd3157544371fe3af8080e0a1c3d51e01f1
Yousuf Elkhoga <yousuf.elkhoga@gmail.com>
2023-03-20 17:00:24 -0400

    structure set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Dash from "./Dash.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -19,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count isn;t {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -27,9 +28,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Dash />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/LinkSidebar.jsx
@@ -0,0 +1,9 @@
+function LinkSidebar() {
+  return (
+    <div className="LinkSidebar">
+      <h3>LinkSidebar</h3>
+    </div>
+  );
+}
+
+export default LinkSidebar;
